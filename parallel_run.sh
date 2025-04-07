0da8a68c346bc479eacce104e8b88b02e947e498
aaw126 <3384370178@qq.com>
2025-04-07 15:33:57 +0800

    Add files via upload

---
+++ parallel_run.sh
@@ -0,0 +1,10 @@
+python extract_face_pipeline.py --input_dir Group_01 --output_dir group1 &
+python extract_face_pipeline.py --input_dir Group_02 --output_dir group2 &
+python extract_face_pipeline.py --input_dir Group_03 --output_dir group3 &
+python extract_face_pipeline.py --input_dir Group_04 --output_dir group4 &
+python extract_face_pipeline.py --input_dir Group_05 --output_dir group5 &
+python extract_face_pipeline.py --input_dir Group_06 --output_dir group6 &
+python extract_face_pipeline.py --input_dir Group_07 --output_dir group7 &
+python extract_face_pipeline.py --input_dir Group_08 --output_dir group8 &
+python extract_face_pipeline.py --input_dir Group_09 --output_dir group9 &
+python extract_face_pipeline.py --input_dir Group_10 --output_dir group10 &
